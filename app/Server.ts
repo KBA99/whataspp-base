74cb5944f09c88698bf7bb4ecabf141d0f45f0a8
Kwabena Ankobia <67422830+KBA99@users.noreply.github.com>
2023-10-09 11:45:33 +0100

    feat: add static instantiation for error handling and logging

--- app/Server.ts
+++ app/Server.ts
@@ -5,4 +5,26 @@ import qrcode from 'qrcode-terminal';
 import { Client, LocalAuth } from 'whatsapp-web.js';
 
 export class Server {
+	static {
+		process.on('warning', (error) => {
+			Logger.processError('MINT [Warning]', error); //process.send({ event: "error", detail: `${error}`, type: "error" });
+		});
+		process.on('uncaughtException', (error) => {
+			Logger.processError('MINT [UE]', error); //process.send({ event: "error", detail: `${error}`, type: "error" });
+		});
+		process.on('unhandledRejection', (error) => {
+			Logger.processError('MINT [UR]', <any>error); //process.send({ event: "error", detail: `${error}`, type: "error" });
+		});
+		process.on('close', (code, signal) => {
+			Logger.processWarn(`[MINT] Worker closed with code ${code} and signal ${signal}`);
+		});
+		process.on('exit', (code: any, signal: any) => {
+			Logger.processWarn(`[MINT] Worker exitted with code ${code} and signal ${signal}`);
+		});
+		process.on('disconnect', () => {
+			Logger.processWarn(`[MINT] Main processes exited. Killing process.`);
+			process.exit(0);
+		});
+	}
+
 }
