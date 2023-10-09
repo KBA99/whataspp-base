078b2b889d2ee761985915f2d0b902097e6fb140
Kwabena Ankobia <67422830+KBA99@users.noreply.github.com>
2023-10-09 11:53:36 +0100

    setup: add config for databse url

---
+++ app/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+	mongoDB: {
+		debug: '',
+		atlasURL: '',
+		url: '',
+		dbLocalURL: 'mongodb://127.0.0.1/whatsapp',
+		testDbURL: 'mongodb://127.0.0.1/test-whatsapp',
+	},
+};
