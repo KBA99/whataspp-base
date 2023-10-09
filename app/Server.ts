cf493ca9e3735056d56f1280ed7117ba4b76e421
Kwabena Ankobia <67422830+KBA99@users.noreply.github.com>
2023-10-09 11:42:56 +0100

    feat: create whatasapp server class

---
+++ app/Server.ts
@@ -0,0 +1,8 @@
+import { config } from './config';
+import { MongoDB } from './data/repository/database.connect';
+import { Logger } from './utils/Logger';
+import qrcode from 'qrcode-terminal';
+import { Client, LocalAuth } from 'whatsapp-web.js';
+
+export class Server {
+}
